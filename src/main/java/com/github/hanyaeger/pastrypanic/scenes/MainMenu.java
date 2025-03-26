6956968d04fd221564a39bdefd0695db60a226ed
Toasty-criminale <JM.vankuijk@student.han.nl>
2025-03-26 11:46:58 +0100

    Background

--- src/main/java/com/github/hanyaeger/pastrypanic/scenes/MainMenu.java
+++ src/main/java/com/github/hanyaeger/pastrypanic/scenes/MainMenu.java
@@ -1,12 +1,16 @@
 package com.github.hanyaeger.pastrypanic.scenes;
 
+import com.github.hanyaeger.api.AnchorPoint;
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.impl.TextEntity;
 import com.github.hanyaeger.api.scenes.StaticScene;
 import com.github.hanyaeger.pastrypanic.PastryPanic;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class MainMenu extends StaticScene {
 
     PastryPanic game;
-
 
     public MainMenu(PastryPanic game) {
         this.game = game;
@@ -14,6 +18,7 @@ public class MainMenu extends StaticScene {
 
     @Override
     public void setupScene() {
+        setBackgroundImage("backgrounds/PastryPanic.png");
 
     }
 
