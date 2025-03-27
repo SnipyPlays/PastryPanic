af7f2c949e88a7bcbd44dcd2a30d25091c4ad0ac
Toasty-criminale <JM.vankuijk@student.han.nl>
2025-03-27 09:29:41 +0100

    Movement van speler toegevoegd

--- src/main/java/com/github/hanyaeger/pastrypanic/entities/characters/Speler.java
+++ src/main/java/com/github/hanyaeger/pastrypanic/entities/characters/Speler.java
@@ -1,9 +1,40 @@
 package com.github.hanyaeger.pastrypanic.entities.characters;
 
 import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.userinput.KeyListener;
+import javafx.scene.input.KeyCode;
 
-public class Speler extends Character{
-    protected Speler(String resource, Coordinate2D initialLocation) {
+import java.util.Set;
+
+public class Speler extends Character implements KeyListener {
+    public Speler(String resource, Coordinate2D initialLocation) {
         super(resource, initialLocation);
+    }
+
+    @Override
+    public void onPressedKeysChange(Set<KeyCode> pressedKeys) {
+        if(pressedKeys.contains(KeyCode.LEFT)){
+            setMotion(3,270d);
+        } else if(pressedKeys.contains(KeyCode.RIGHT)){
+            setMotion(3,90d);
+        } else if(pressedKeys.contains(KeyCode.UP)){
+            setMotion(3,180d);
+        } else if(pressedKeys.contains(KeyCode.DOWN)){
+            setMotion(3,0d);
+        }  else if(pressedKeys.contains(KeyCode.D)){
+            setMotion(3,90d);
+        }
+        else if(pressedKeys.contains(KeyCode.A)){
+            setMotion(3,270d);
+        }
+        else if(pressedKeys.contains(KeyCode.W)){
+            setMotion(3,180d);
+        }
+        else if(pressedKeys.contains(KeyCode.S)){
+            setMotion(3,0d);
+        }
+        else if (pressedKeys.isEmpty()){
+            setSpeed(0);
+        }
     }
 }
--- src/main/java/com/github/hanyaeger/pastrypanic/scenes/GameScene.java
+++ src/main/java/com/github/hanyaeger/pastrypanic/scenes/GameScene.java
@@ -1,6 +1,8 @@
 package com.github.hanyaeger.pastrypanic.scenes;
 
+import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.scenes.DynamicScene;
+import com.github.hanyaeger.pastrypanic.entities.characters.Speler;
 
 public class GameScene extends DynamicScene {
     @Override
@@ -10,6 +12,7 @@ public class GameScene extends DynamicScene {
 
     @Override
     public void setupEntities() {
-
+        var speler = new Speler("sprites/hanny.png", new Coordinate2D(getWidth()/2, getHeight()/2));
+        addEntity(speler);
     }
 }
